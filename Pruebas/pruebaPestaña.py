60bc0783e27f2118ee406778316ebfb6daa1d847
DiegoGajardo01 <diegogajardo1@gmail.com>
2022-11-05 01:14:33 -0300

    Formulario cliente con ingreso a bd y prueba de estilo(Does not work)

--- Pruebas/pruebaPestaña.py
+++ Pruebas/pruebaPestaña.py
@@ -1,5 +1,5 @@
 from tkinter import *
-from tkinter import ttk
+from tkinter.ttk import Notebook, Style
 
 raiz =Tk()
 raiz.title("Onesco")
@@ -8,12 +8,30 @@ raiz.config(bg ="gray")
 raiz.config(bd="30")
 raiz.config(relief="groove")
 
-
-nb = ttk.Notebook(raiz)
+nb = Notebook(raiz)
 nb.pack(fill='both', expand='yes')
-pes0 = ttk.Frame(nb)
-pes1 = ttk.Frame(nb)
+
+pes0 = Frame(nb)
+pes1 = Frame(nb)
 
 nb.add(pes0, text="Clientes")
 nb.add(pes1, text="Autos")
+
+
+Mysky = "#dac0f0"
+Myyellow = "#F2C84B"
+
+style = Style()
+
+
+style.theme_create( "dummy", parent="alt", settings={
+        "TNotebook": {"configure": {"tabmargins": [2, 5, 2, 0] } },
+        "TNotebook.Tab": {
+            "configure": {"padding": [200, 1], "background": Mysky },
+            "map":       {"background": [("selected", Myyellow)],
+                          "expand": [("selected", [1, 1, 1, 0])] } } } )
+
+style.theme_use("dummy")
+
+
 raiz.mainloop()
--- formulario.py
+++ formulario.py
@@ -1,5 +1,5 @@
 from tkinter import *
-from tkinter import ttk
+from tkinter.ttk import Notebook, Style, Treeview
 from tkinter import messagebox
 import mysql.connector
 import ventanas
@@ -69,14 +69,62 @@ def formularioRecepcion():
     raiz.config(bd="30")
     raiz.config(relief="groove")
 
-    nb = ttk.Notebook(raiz)
+
+    nb = Notebook(raiz)
     nb.pack(fill='both', expand='yes')
-    pes0 = ttk.Frame(nb)
-    pes1 = ttk.Frame(nb)
+
+    pes0 = Frame(nb)
+    pes1 = Frame(nb)
 
     nb.add(pes0, text="Clientes")
     nb.add(pes1, text="Autos")
-    
+
+
+    def volver():
+        raiz.withdraw()
+
+    #Formulario Cliente
+    rutlablel = Label(pes0,text ="RUT:",font =(16))
+    rutlablel.grid(row="1",column="0")
+    nombreslabel = Label(pes0,text ="Nombre:",font =(16))
+    nombreslabel.grid(row="2",column="0")
+    contactolabel = Label(pes0,text ="Contacto:",font =(16))
+    contactolabel.grid(row="3",column="0")
+
+
+    textRut= Entry(pes0,width="30")
+    textRut.grid(row="1",column="1")
+    textNombres= Entry(pes0,width="30")
+    textNombres.grid(row="2",column="1")
+    textContacto= Entry(pes0,width="30")
+    textContacto.grid(row="3",column="1")
+
+    def send_data_cliente():
+        Rut = textRut.get()
+        Nombres = textNombres.get()
+        Contacto = textContacto.get()
+        
+        cursor1=conexion1.cursor()
+        sql = "INSERT INTO `onesco_recepcion_clientes`(`rut_cliente`, `nombre_cliente`, `contacto`) VALUES (%s,%s,%s)"
+        datos=(Rut, Nombres, Contacto)
+        cursor1.execute(sql, datos)
+        conexion1.commit()
+        conexion1.close()
+        textRut.delete(0, END)
+        textNombres.delete(0, END)
+        textContacto.delete(0, END)
+        #textChasis.focus()  <-- Para hacer que esté esperando texto en chasis una vez enviado
+        
+
+    #Botones
+    volverButton = Button(pes0, text="Volver", command=volver)
+    volverButton.grid(column=7, row=3,ipadx=5, ipady=5, padx=10, pady=10)
+
+    ingresarboton = Button(pes0, text="Guardar Datos", command=send_data_cliente)
+    ingresarboton.grid(row=10, column=4)
+
+    #=======================================================================================#
+    #Formulario Auto
     chasislable = Label(pes1,text ="Chasis:",font =(16))
     chasislable.grid(row="1",column="0")
     patentelabel = Label(pes1,text ="Patente:",font =(16))
@@ -87,8 +135,12 @@ def formularioRecepcion():
     marcalabel.grid(row="4",column="0")
     añolabel = Label(pes1,text ="Año:",font =(16))
     añolabel.grid(row="5",column="0")
+    klmlabel = Label(pes1,text ="Kilometraje:",font =(16))
+    klmlabel.grid(row="6",column="0")
     preciolabel = Label(pes1,text ="Precio:",font =(16))
-    preciolabel.grid(row="6",column="0")
+    preciolabel.grid(row="7",column="0")
+    rutDulabel = Label(pes1,text ="Rut Dueño:",font =(16))
+    rutDulabel.grid(row="8",column="0")
 
 
     textChasis= Entry(pes1,width="30")
@@ -101,24 +153,28 @@ def formularioRecepcion():
     textMarca.grid(row="4",column="1")
     textAño= Entry(pes1,width="30")
     textAño.grid(row="5",column="1")
+    textKlm= Entry(pes1,width="30")
+    textKlm.grid(row="6",column="1")
     textPrecio= Entry(pes1,width="30")
-    textPrecio.grid(row="6",column="1")
+    textPrecio.grid(row="7",column="1")
+    textRutDu= Entry(pes1,width="30")
+    textRutDu.grid(row="8",column="1")
     
-    def volver():
-        raiz.withdraw()
 
     
-    def send_data():
+    def send_data_auto():
         Chasis = textChasis.get()
         Patente = textPatente.get()
         Modelo = textModelo.get()
         Marca = textMarca.get()
         Año = textAño.get()
+        KiloM = textKlm.get()
         Precio = textPrecio.get()
+        RutDu = textRutDu.get()
         if Año.isdigit() == True and Precio.isdigit() == True:
             cursor1=conexion1.cursor()
-            sql = "INSERT INTO `onesco_autos`(`chasis`, `patente`, `modelo`, `marca`, `año`, `precio`) VALUES (%s,%s,%s,%s,%s,%s)"
-            datos=(Chasis,Patente,Modelo,Marca,Año,Precio)
+            sql = "INSERT INTO `onesco_autos`(`chasis`, `patente`, `modelo`, `marca`, `año`, `kilometraje`, `precio`, `rut_dueño`) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
+            datos=(Chasis,Patente,Modelo,Marca,Año, KiloM, Precio, RutDu)
             cursor1.execute(sql, datos)
             conexion1.commit()
             conexion1.close()
@@ -127,7 +183,9 @@ def formularioRecepcion():
             textModelo.delete(0, END)
             textMarca.delete(0, END)
             textAño.delete(0, END)
+            textKlm.delete(0, END)
             textPrecio.delete(0, END)
+            textRutDu.delete(0, END)
             #textChasis.focus()  <-- Para hacer que esté esperando texto en chasis una vez enviado
         else:
             messagebox.showinfo(message="Hemos tenido problemas al intentar ingresar datos, por favor en AÑO y PRECIO introduce SOLO VALORES NUMERICOS, Sin Usar ni puntos ni comas", title="Problemas para ingresar datos")
@@ -141,7 +199,7 @@ def formularioRecepcion():
     printDataButton = Button(pes1, text="Mostrar Datos", command=tablaRecepcion)
     printDataButton.grid(row="10", column="3")
 
-    ingresarboton = Button(pes1, text="Guardar Datos", command=send_data)
+    ingresarboton = Button(pes1, text="Guardar Datos", command=send_data_auto)
     ingresarboton.grid(row="10", column="4")
     raiz.mainloop()
 
@@ -158,7 +216,7 @@ def tablaRecepcion():
     def volver():
         raiz.withdraw()
 
-    lista = ttk.Treeview( miframe, columns=(1,2,3,4,5,6,7), show="headings", height="8")
+    lista = Treeview( miframe, columns=(1,2,3,4,5,6,7,8), show="headings", height="8")
     
     lista.column(1, width=100)
     lista.column(2, width=100)
@@ -167,14 +225,16 @@ def tablaRecepcion():
     lista.column(5, width=100)
     lista.column(6, width=100)
     lista.column(7, width=100)
+    lista.column(8, width=100)
 
     lista.heading(1, text="Chasis", anchor=CENTER)
     lista.heading(2, text="Patente", anchor=CENTER)
     lista.heading(3, text="Modelo", anchor=CENTER)
     lista.heading(4, text="Marca", anchor=CENTER)
     lista.heading(5, text="Año", anchor=CENTER)
-    lista.heading(6, text="Precio", anchor=CENTER)
-    lista.heading(7, text="Dueño", anchor=CENTER)
+    lista.heading(6, text="Kilometraje", anchor=CENTER)
+    lista.heading(7, text="Precio", anchor=CENTER)
+    lista.heading(8, text="Dueño", anchor=CENTER)
     
     conexion1=mysql.connector.connect(host="localhost", 
                                   user="root", 
@@ -182,7 +242,7 @@ def tablaRecepcion():
                                   database="onesco")
 
     cursor1=conexion1.cursor()
-    sql = "select * from `onesco_autos`"
+    sql = "SELECT `chasis`, `patente`, `modelo`, `marca`, `año`, `kilometraje`, `precio`, `rut_dueño` FROM `onesco_autos` WHERE 1;"
     cursor1.execute(sql)
     a = cursor1.fetchall()
     for i in a:
